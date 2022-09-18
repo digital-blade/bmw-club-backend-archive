6da4e7ea9a41527dbcc63c8520e27cd079ca61ba
Danil Tankov <danil.tankoff@yandex.ru>
2022-09-19 01:17:18 +0700

    fix: eureka-server должна зависеть от config-server

--- services/config-server/src/main/java/com/dblade/bmw/config/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/dblade/bmw/config/ConfigServerApplication.java
@@ -3,10 +3,8 @@ package com.dblade.bmw.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
   public static void main(String[] args) {
